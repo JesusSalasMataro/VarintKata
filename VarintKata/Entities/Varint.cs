1297cb2d4adf59bd80d739bdcce5ad165a5be9e4
JesusSalasMataro <jesus459@hotmail.es>
2016-08-28 23:34:32 +0200

    First iteration

---
+++ VarintKata/Entities/Varint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarintKata.Entities
+{
+    public class Varint
+    {
+        public string ConvertVarToInt(int number)
+        {
+            return "00000001";
+        }
+    }
+}
---
+++ VarintKataTests/VarintUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VarintKata.Entities;
+
+namespace VarintKataTests
+{
+    [TestClass]
+    public class VarintUnitTests
+    {
+        [TestMethod]
+        public void Given_SingleByteInt_When_ConvertToVarint_Then_ReturnCorrectResult()
+        {
+            // ARRANGE
+            Varint varint = new Varint();
+
+            // ACT
+            string binaryRepresentationActual = varint.ConvertVarToInt(1);
+            string binaryRepresentationExpected = "00000001";
+
+            // ASSERT
+            Assert.AreEqual(binaryRepresentationExpected, binaryRepresentationActual);
+        }
+    }
+}
