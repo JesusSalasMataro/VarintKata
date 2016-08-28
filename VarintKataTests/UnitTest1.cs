089497851a56a80fb4c495b21b1e5814b23694c9
JesusSalasMataro <jesus459@hotmail.es>
2016-08-28 23:23:46 +0200

    Init

---
+++ VarintKataTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VarintKataTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Given_SingleByteInt_When_ConvertToVarint_Then_ReturnCorrectResult()
+        {
+
+        }
+    }
+}
